c5bac604e3ac0b54b220b68a0e60eb5877130dbb
Peter M. Elias <petermelias@gmail.com>
2013-09-08 02:53:03 -0500

    long description

--- setup.py
+++ setup.py
@@ -1,20 +1,3 @@
-"""
-Flask EasyMode
------------------
-
-This extension is a modular extension (meaning you can turn features on and off) designed
-to provide modules of useful helpers, decorators, and mixins to make your Flask web app
-development experience even easier than it already is thanks to Flask's good design.
-
-The purpose of this extension is not to turn Flask's modular nature into something monolithic.
-
-Every feature of this extension can be enabled or disabled at will and most features are disabled
-by default.
-
-Initializing this extension does very little other than associate the application.
-	
-"""
-
 from setuptools import setup, find_packages
 
 setup(
@@ -25,7 +8,7 @@ setup(
 	author='Peter M. Elias',
 	author_email='petermelias@gmail.com',
 	description='Make Flask development even easier',
-	long_description=__doc__,
+	long_description=open('README.md').read(),
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
