aab9ce8719dfdef078d39d075485a6a693e256aa
Peter M. Elias <petermelias@gmail.com>
2013-09-26 15:46:15 -0700

    micro bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='flask-easymode',
-	version='0.0.9',
+	version='0.0.10',
 	url='http://github.com/petermelias/flask-easymode',
 	license='BSD',
 	author='Peter M. Elias',
